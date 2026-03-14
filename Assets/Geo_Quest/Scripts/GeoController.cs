6ee5c2c47ea04d18351b755f93625523a7d89de5
AidenSTEMcoding <aidencardona@unityprep.org>
2026-03-14 16:19:48 -0400

    GeoQuest MidTerm

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -7,11 +7,13 @@ public class GeoController : MonoBehaviour
 {
     private Rigidbody2D rb;
     public int speed = 5;
+    private SpriteRenderer sr;
     public string nextlevel = "Scene_2";
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -20,6 +22,10 @@ public class GeoController : MonoBehaviour
 
         float xInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Alpha1)) sr.color = Color.cyan;
+        if (Input.GetKeyDown(KeyCode.Alpha2)) sr.color = Color.green;
+        if (Input.GetKeyDown(KeyCode.Alpha3)) sr.color = new Color(0f, 0.5f, 1f); // Custom Blue
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
